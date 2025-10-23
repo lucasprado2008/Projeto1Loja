7500766c6a83c1b4e2f950a61a77a7f88121b26a
Lucas <lucashenriquecardosoprado@gmail.com>
2025-10-23 14:37:13 -0300

    2 - Criando o banco de dados

---
+++ script.sql
@@ -0,0 +1,21 @@
+-- CRIANDO O BANCO DE DADOS
+CREATE DATABASE dbProjeto1Loja;
+
+-- USANDO O BANCO DE DADOS
+USE dbProjeto1Loja;
+
+-- CRIANDO AS TABELAS
+CREATE TABLE tbUsuario(
+	idUsuario int primary key auto_increment,
+    nome varchar(40) not null,
+    email varchar(40) not null,
+    senha varchar(40) not null
+);
+
+CREATE TABLE tbProduto(
+	idProduto int primary key auto_increment,
+    nome varchar(40) not null,
+    descricao varchar(80) not null,
+    preco decimal(8,2) not null,
+    quantidade int not null
+);
