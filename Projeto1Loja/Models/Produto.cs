2230174032979f5873268e2fb34d7901eb4e2844
Lucas <lucashenriquecardosoprado@gmail.com>
2025-10-23 14:55:04 -0300

    4 - Configurando as models usuario e produto

---
+++ Projeto1Loja/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace Projeto1Loja.Models
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public decimal Quantidade { get; set; }
+    }
+}
---
+++ Projeto1Loja/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace Projeto1Loja.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string email {get; set; }
+        public string senha { get; set; }
+    }
+}
