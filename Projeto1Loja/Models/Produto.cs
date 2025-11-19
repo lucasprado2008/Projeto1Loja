2730a89283ba489abb962544e61c0f8c1a286f7a
Lucas <lucashenriquecardosoprado@gmail.com>
2025-11-18 22:01:16 -0300

    15 - Finalizando o projeto e gravando o vídeo

--- Projeto1Loja/Models/Produto.cs
+++ Projeto1Loja/Models/Produto.cs
@@ -6,6 +6,6 @@
         public string? nome { get; set; }
         public string? descricao { get; set; }
         public decimal preco { get; set; }
-        public decimal quantidade { get; set; }
+        public int quantidade { get; set; }
     }
 }
